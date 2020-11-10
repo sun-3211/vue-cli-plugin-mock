516511ec437eabdfceee721af0512e7fcc2b7677
Gogs <gogs@fake.local>
2020-11-10 14:04:29 +0800

    初始化项目

--- mock/index.js
+++ mock/index.js
@@ -7,35 +7,40 @@ let webpackWatch = require('./webpackWatch');
 let isDebug = false;
 
 module.exports = function (options, useWebpack) {
-  options = options || {};
-  let entry = options.entry || './mock/index.js';
-  isDebug = options.debug;
+    options = options || {};
+    let entry = './mock/';
+    isDebug = options.debug;
 
-  if (path.isAbsolute(entry) === false) {
-    entry = path.resolve(process.cwd(), entry);
-  }
-  let watchConfig = { entry: entry, interval: options.interval || 200 };
-
-  if (useWebpack) {
-    isDebug && logcat.debug('use webpack watch mock file.');
-    webpackWatch(watchConfig, refreshMock);
-  } else {
-    isDebug && logcat.debug('use fs watchFile mock file.');
-    fsWatch(watchConfig, refreshMock);
-  }
-
-  return apiMocker({}, mocklogFn);
-
-  function refreshMock(mockObj) {
-    apiMocker.refresh(mockObj);
-    logcat.log('Done: Hot Mocker file replacement success!');
-  }
-
-  function mocklogFn(type, msg) {
-    if (type === 'matched') {
-      logcat.log(type + ' ' + msg);
-    } else {
-      isDebug && logcat.debug(type + ' ' + msg);
+    if (path.isAbsolute(entry) === false) {
+        entry = path.resolve(process.cwd(), entry);
     }
-  }
+    console.log(entry);
+    if (!path.isDirectory(entry)) {
+        logcat.log("未创建mock目录");
+        return;
+    }
+    let watchConfig = {entry: entry, interval: options.interval || 200};
+
+    if (useWebpack) {
+        isDebug && logcat.debug('use webpack watch mock file.');
+        webpackWatch(watchConfig, refreshMock);
+    } else {
+        isDebug && logcat.debug('use fs watchFile mock file.');
+        fsWatch(watchConfig, refreshMock);
+    }
+
+    return apiMocker({}, mocklogFn);
+
+    function refreshMock(mockObj) {
+        apiMocker.refresh(mockObj);
+        logcat.log('Done: Hot Mocker file replacement success!');
+    }
+
+    function mocklogFn(type, msg) {
+        if (type === 'matched') {
+            logcat.log(type + ' ' + msg);
+        } else {
+            isDebug && logcat.debug(type + ' ' + msg);
+        }
+    }
 };
