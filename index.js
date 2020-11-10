4e63d4419d5789fe8945cd7589e813fe2a7c6747
Gogs <gogs@fake.local>
2020-11-10 16:41:20 +0800

    项目完成

--- index.js
+++ index.js
@@ -2,19 +2,23 @@ const MockMiddleware = require('./mock');
 const webpackWatch = require('./mock/webpackWatch');
 const logger = require('./mock/logger');
 module.exports = (api, options) => {
+    const webpack = require(api.resolve('node_modules/webpack'));
+    webpackWatch.configWebpack(webpack);
+    const mockOptions = options.pluginOptions && options.pluginOptions.mock || {};
+    if (mockOptions.disable) {
+        logger.log('mock middleware disabled!');
+        return;
+    }
+    let entry = './mock/';
+    mockOptions.entry = api.resolve(entry);
     if (process.env.NODE_ENV === 'development') {
-        const webpack = require(api.resolve('node_modules/webpack'));
-        webpackWatch.configWebpack(webpack);
-        const mockOptions = options.pluginOptions && options.pluginOptions.mock || {};
-        if (mockOptions.disable) {
-            logger.log('mock middleware disabled!');
-            return;
-        }
-        let entry = './mock/';
-        mockOptions.entry = api.resolve(entry);
-        // logger.log(mockOptions);
         api.configureDevServer(function (app, server) {
             app.use(MockMiddleware(mockOptions, true));
         });
+    } else if (options.debug) {
+        // logger.log(mockOptions);
+        webpackWatch(mockOptions, function (data) {
+            console.log("====>", data);
+        })
     }
 };
