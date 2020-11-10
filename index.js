9e9843c23b9107901f28c4a8cb300422c96d821d
Gogs <gogs@fake.local>
2020-11-10 12:11:21 +0800

    初始化项目

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = (api, options) => {
     api.chainWebpack(config => {
-        const before = config.devServer.get("before");
+        // const before = config.devServer.get("before");
         config.devServer.before(app => {
-            before(app);
+            // before(app);
             app.get('/test', function (req, res) {
                 // res.json({aaa: "bbb"});
                 res.end("testtesttesttesttest");
