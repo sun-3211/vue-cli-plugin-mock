d3062e6af35d66f01b6cbeed6f004775a6554804
Gogs <gogs@fake.local>
2020-11-10 17:27:30 +0800

    项目完成

--- generator/index.js
+++ generator/index.js
@@ -1,7 +1,4 @@
 module.exports = api => {
     console.log(api);
-    api.injectImports(api.entryFile, `
-    import MockUtil from 'vue-cli-plugin-mock/mock/local_util.js'
-    window.mockUtil = new MockUtil();
-    `);
+    api.injectImports(api.entryFile, `import 'vue-cli-plugin-mock/mock/local_util.js'`);
 }
