f75df00182eef9323b46187511e1752b19d67e30
Gogs <gogs@fake.local>
2020-11-10 12:03:36 +0800

    初始化项目

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 module.exports = (api, options) => {
+    console.log("api");
     api.chainWebpack(config => {
+        console.log("config");
         config.devServer.before(app => {
-            console.log(app);
+            app.get('/test', function (req, res) {
+                // res.json({aaa: "bbb"});
+                res.end("aaaaaaaaaaaaaaaa");
+            });
         });
         // console.log(config.devServer);
     });
